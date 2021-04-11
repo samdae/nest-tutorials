a64c903801844f0a905de4050d3af90fc18e6bcc
samdae3@naver.com <samdae3@naver.com>
2021-04-11 20:45:05 +0900

    SI dauhun_lee
    constructor / validate / transform / Param / Body :  learned

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,30 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+ /*
+  *  Spring에서의 Di
+  *  bena factory 처럼 관리 해주는 듯 함 ~~
+  */
   constructor(private readonly appService: AppService) {}
 
+
+  /**
+   *   Request mapping 하는 부분인가봄~~
+   *   getMapping 을 그냥 이렇게 쓰나봄 
+   */
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+
+  /**
+   *        typescript => 는 한정자가 없나? 
+   *        thwos는 어디서 하나, try catch 면 테스트 툴 돌릴때 노답인디,
+   */
+  @Get('/hello')
+  sayHello(): string {
+    return this.appService.getHi();
+  }
+  
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // valid whitelist 사용 여부 
+      forbidNonWhitelisted: true, // whitelist 아닌 내용 input 금지 여부
+      transform: true, // 자동 형변환 
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
